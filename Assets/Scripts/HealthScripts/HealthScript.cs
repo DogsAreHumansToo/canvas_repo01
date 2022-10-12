5d547336ad40005b9e72f2829b25d44235db12b9
Erik <erikjohansson1999@outlook.co.nz>
2022-10-12 15:19:22 +1300

    Added Lose Condition and Game Over/ Retry

--- Assets/Scripts/HealthScripts/HealthScript.cs
+++ Assets/Scripts/HealthScripts/HealthScript.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class HealthScript : MonoBehaviour
 {
+    public static event Action onPlayerDeath;
+
     [SerializeField] private float startingHealth;
     public float currentHealth { get; private set; }
 
@@ -16,14 +19,13 @@ public class HealthScript : MonoBehaviour
     {
         currentHealth = Mathf.Clamp(currentHealth - _Damage, 0, startingHealth);
 
-        if(currentHealth > 0)
+        if(currentHealth <= 0)
         {
-
+            //currentHealth = 0;
+            
+            onPlayerDeath?.Invoke();
         }
-        else
-        {
-
-        }
+        
     }
 
     private void Update()
@@ -33,4 +35,5 @@ public class HealthScript : MonoBehaviour
             TakeDamage(1);
         }
     }
+
 }
