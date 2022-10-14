cf0151f005718a7f987560461360671318534831
DogsAreHumansToo <marc_laban@hotmail.com>
2022-10-14 14:50:39 +1300

    ReAdded attackspeed buff

--- Assets/Scripts/PlayerScripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerScripts/PlayerCombat.cs
@@ -13,18 +13,20 @@ public class PlayerCombat : MonoBehaviour
     public float attackRange = 0.5f;
     public int attackDamage = 50;
     
-    public float attackRate = 3f;
+    public float attackRate = 4f;
     float nextAttackTime = 0f;
 
     // Update is called once per frame
     void Update()
     {
-        if(Time.time>= nextAttackTime)
+        if(Time.time >= nextAttackTime)
         {
             if (Input.GetKeyDown(KeyCode.K))
             {
                 Attack();
                 nextAttackTime = Time.time + 1f / attackRate;
+                Debug.Log("attack");
+                
             }
         }
     }
---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    public float multiplier = 2;
+    public float duration = 4;
+
+    //public GameObject pickupEffect;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine(Pickup(other));
+        }
+    }
+    IEnumerator Pickup(Collider2D player)
+    {
+        Debug.Log("Picked UP!");
+        PlayerCombat stats = player.GetComponent<PlayerCombat>();
+        stats.attackRate *= multiplier;
+
+        GetComponent<SpriteRenderer>().enabled = false;
+        GetComponent<Collider2D>().enabled = false;
+
+        yield return new WaitForSeconds(duration);
+
+        stats.attackRate /= multiplier;
+
+        Destroy(gameObject);
+    }
+
+}
