382c0250c50d929de589218eacc1e4e59417dc23
Erik <erikjohansson1999@outlook.co.nz>
2022-10-21 16:15:59 +1300

    Formatting

--- Assets/Scripts/SystemScripts/GameSystem.cs
+++ Assets/Scripts/SystemScripts/GameSystem.cs
@@ -10,11 +10,15 @@ public class GameSystem : MonoBehaviour
     public bool waveLevel = false;
     public int enemiesKilled = 0;
 
-    public void LevelWaveComplete()
+    private void Update()
     {
         if (waveLevel)
         {
-            
+            if (enemiesKilled >= 7)
+            {
+                Debug.Log("WIN!!!!!!!!!!!!!!!");
+                enemiesKilled = 0;
+            }
         }
     }
 }
