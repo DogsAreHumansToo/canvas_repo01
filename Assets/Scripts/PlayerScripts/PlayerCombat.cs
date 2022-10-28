b1f37ce0d36cc8a20d98a99a19400ab50022248f
DogsAreHumansToo <marc_laban@hotmail.com>
2022-10-28 16:55:31 +1300

    Build

--- Assets/Scripts/PlayerScripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerScripts/PlayerCombat.cs
@@ -35,7 +35,7 @@ public class PlayerCombat : MonoBehaviour
     {
         if(Time.time >= nextAttackTime)
         {
-            if (Input.GetKeyDown(KeyCode.K))
+            if (Input.GetKeyDown(KeyCode.Mouse0))
             {
                 Attack();
                 nextAttackTime = Time.time + 1f / attackRate;
