e14468ce59a8497c32dfaeeb07a24e1c19ea3741
DogsAreHumansToo <marc_laban@hotmail.com>
2022-10-28 16:18:52 +1300

    Added Boss and spawn after enemies killed

--- Assets/Scripts/AbilityScripts/Dash.cs
+++ Assets/Scripts/AbilityScripts/Dash.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class Dash : Ability
 {
     public float dashVelocity;
+    [SerializeField] bool canUseAbility = false;
 
     public override void Activate(GameObject parent)
     {
---
+++ Assets/Scripts/EnemyScripts/BossHitBox.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class BossHitBox : MonoBehaviour
+{
+    public int maxHealth = 300;
+    int currentHealth;
+    public GameSystem gameSystem;
+
+    PlayerCombat killCount;
+    private EnemySpawnerFixed enemySpawner;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        //play HURT animation
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+        
+    }
+
+    public void Die()
+    {
+        if(gameSystem == null)
+        {
+            gameSystem = FindObjectOfType<GameSystem>();
+        }
+        Debug.Log("Enemy died!");
+
+        
+        //Debug.Log(gameSystem.enemiesKilled);
+
+        //enemySpawner = FindObjectOfType<EnemySpawnerFixed>();
+        //enemySpawner.enemiesInRoom--;
+        
+        
+        //if (EnemySpawnerFixed.spawnTime <= 0 && enemySpawner.enemiesInRoom <= 0)
+        //{
+        //    enemySpawner.spawnerDone = true;
+        //}
+
+        //Die Animation
+
+        //Disable the enemy
+
+        //gameSystem.enemiesKilled++;
+        Destroy(gameObject);
+    }
+
+
+}
