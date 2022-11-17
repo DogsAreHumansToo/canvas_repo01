f39f5b39b4186b31a6052d3c1357950e75a72499
DogsAreHumansToo <marc_laban@hotmail.com>
2022-11-18 06:57:29 +1300

    Debris Does Damage

--- Assets/Scripts/Debris/Debris.cs
+++ Assets/Scripts/Debris/Debris.cs
@@ -8,9 +8,19 @@ public class Debris : MonoBehaviour
     public float RotationSpeed;
     public bool Clockwise;
 
+    public PlayerMovementScript playerMovement;
+
 
     public DebrisSpawner _spawner;
+    private ScreenShakeController cameraShake;
+    [SerializeField] private GameObject player;
+    [SerializeField] private float damage;
 
+    private void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        cameraShake = FindObjectOfType<ScreenShakeController>();
+    }
     private void Update()
     {
         if(Clockwise==false)
@@ -24,6 +34,7 @@ public class Debris : MonoBehaviour
 
         transform.rotation = Quaternion.Euler(0, 0, rotZ);
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("nextLine"))
@@ -34,5 +45,26 @@ public class Debris : MonoBehaviour
         {
             Destroy(this.gameObject);
         }
+        playerMovement = player.GetComponent<PlayerMovementScript>();
+        if (collision.gameObject.tag == "Player")
+        {
+
+
+            playerMovement.KBCounter = playerMovement.KBTotalTime;
+            if (collision.transform.position.x < transform.position.x)
+            {
+                playerMovement.KnockFromRight = true;
+            }
+            if (collision.transform.position.x > transform.position.x)
+            {
+                playerMovement.KnockFromRight = false;
+            }
+
+
+            StartCoroutine(cameraShake.Shake(.04f, .08f));
+            collision.GetComponent<HealthScript>().TakeDamage(damage);
+
+        }
     }
+
 }
